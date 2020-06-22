4085bfc2a908d5ae3f4531fc150e9c2ad199238e
rgupta <rgupta@travelrepublic.com>
2020-06-22 12:51:00 +0100

    Updates

---
+++ precache-manifest.894fb0bc8cf5e3675aa88535533acb0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af03641479b801b8a9b76ec458e4637c",
+    "url": "/HelloWorld/index.html"
+  },
+  {
+    "revision": "c14be8b7701c3f829235",
+    "url": "/HelloWorld/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ffa2c8fde00042e9ff8d",
+    "url": "/HelloWorld/static/js/2.1aafc397.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HelloWorld/static/js/2.1aafc397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14be8b7701c3f829235",
+    "url": "/HelloWorld/static/js/main.92d14858.chunk.js"
+  },
+  {
+    "revision": "ab59f3bbd9f0d364cec0",
+    "url": "/HelloWorld/static/js/runtime-main.65b3ec5e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/HelloWorld/static/media/logo.ee7cd8ed.svg"
+  }
+]);
